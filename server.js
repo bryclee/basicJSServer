77beca8846fd7cc0d93e0acb2cf89f404bca6a00
Bryan Lee <pygmize.me@gmail.com>
2015-06-18 13:06:17 -0700

    [feature] Adds html page  - Page queries the server for a webpage

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-var app = require('express')(); // for server: http://expressjs.com/starter/hello-world.html
+var express = require('express');
+var app = express(); // for server: http://expressjs.com/starter/hello-world.html
 var http = require('http'); // for making requests: https://nodejs.org/api/http.html#http_http_request_options_callback
 
 var utils = require('./utils/utils.js');
 
-// Function for server to respond on get requests made to '/' route
-// Sends request to google and returns the response to the client
+app.use(express.static('public'));
 app.get('/', function(request, response) {
-  var pageReq = http.get('http://www.google.com', function(httpRes) {
+  response.send('index.html');
+});
+
+var websites = [
+  'http://www.google.com',
+  'http://finance.yahoo.com/q?s=GOOG'
+];
+var websiteCounter = 0;
+
+// Function for server to respond on get requests made to '/data' route
+// Sends request to google and returns the response to the client
+app.get('/data', function(request, response) {
+  var pageReq = http.get(websites[websiteCounter], function(httpRes) {
     utils.getRequestData(httpRes, function(data) {
       response.send(data);
     });
   });
+
+  websiteCounter++;
+  if (websiteCounter === websites.length) {
+    websiteCounter = 0;
+  }
 });
 
 // Set up server on port 3000
